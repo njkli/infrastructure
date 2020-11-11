d36ed66b240286532b0c6670346e74297e182bad
Voob of Doom <vod@njk.li>
2020-11-11 19:46:43 +0100

    better naming

--- deploy/domain/config.nix
+++ deploy/domain/config.nix
@@ -7,21 +7,13 @@ let
   dom = getEnv "DEPLOY_DOMAIN";
   domID_tf = replaceStrings [ "." ] [ "_" ] dom;
 
-  # vultr_record = {
-  #   domain = dom;
-  #   type = "NS";
-  #   name = head (splitString "." dom);
-  #   ttl = 120;
-  # };
-
   required_providers = fromJSON (readFile ../terraform-providers.json);
-
   terraform = {
     backend.artifactory.username = getEnv "ARTIFACTORY_UNAME";
     backend.artifactory.password = getEnv "ARTIFACTORY_PASSWD";
     backend.artifactory.url = getEnv "ARTIFACTORY_URL";
     backend.artifactory.repo = "tfstate";
-    backend.artifactory.subpath = "terraform-bar";
+    backend.artifactory.subpath = "domain-${dom}";
   } // required_providers.terraform;
 
   provider.vultr.api_key = getEnv "VULTR_API_KEY";
@@ -34,34 +26,27 @@ in
     terraform
     provider;
 
+  # TODO: make vultr_dns module
   resource.vultr_dns_domain."${domID_tf}" = {
     domain = dom;
     server_ip = "127.0.0.1";
-    # provisioner.local-exec.command = ""; # NOTE: runs on create only!
     provisioner = [
       {
         local-exec = [
-          { command = ""; }
+          {
+            command = "echo create"; # NOTE: runs on create only!
+          }
         ];
       }
 
       {
         local-exec = [
           {
-            when = "destroy"; # NOTE: runs on associated resource destruction only!
-            command = "";
+            when = "destroy"; # NOTE: runs on destroy only!
+            command = "echo destroy";
           }
         ];
       }
     ];
   };
-
-  resource.null_resource.vultr_dns_domain_destroy_cleanup = {
-    depends_on = [ "vultr_dns_domain.${domID_tf}" ];
-    provisioner.local-exec = {
-      when = "destroy"; # NOTE: runs on associated resource destruction only!
-      command = "";
-    };
-  };
-
 }
