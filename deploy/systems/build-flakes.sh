f7ee1c57f342817f126a35f7ae316847303ac511
Voob of Doom <vod@njk.li>
2020-12-09 14:04:11 +0100

    fts

--- deploy/systems/build-flakes.sh
+++ deploy/systems/build-flakes.sh
@@ -4,13 +4,21 @@
 git clone https://${DEPLOYMENT_KEY}@github.com/njkli/systems.git
 cd systems
 
+echo '******************************'
+nix --version
+echo '******************************'
+
 for system in $(nix eval .#nixosConfigurations --apply builtins.attrNames --json | jq -r 'join("\n")')
 do
+    echo '******************************'
+    echo ${system}
+    echo '******************************'
     nix-store --query --references \
               $(nix build .#nixosConfigurations.${system}.config.system.build.toplevel --json | jq -r 'map(.drvPath) | join("\n")') \
         | xargs nix-store --realise \
         | xargs nix-store --query --requisites \
         | cachix push njk
+    echo '******************************'
 done
 
 # mkdir -p $HOME/.ssh && \
