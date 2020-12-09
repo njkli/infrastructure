fbce823ea0a9eeabe4b807efc6a048e429490326
Voob of Doom <vod@njk.li>
2020-12-09 16:59:08 +0100

    funny shit, yo!

--- deploy/systems/build-flakes.sh
+++ deploy/systems/build-flakes.sh
@@ -1,28 +1,27 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p git jq findutils bash nixFlakes
+#! nix-shell -i bash -p git jq findutils bash nixFlakes curl
+
+_msg() {
+    curl -s \
+         --form-string "token=${PUSHOVER_TOKEN}" \
+         --form-string "user=${PUSHOVER_USER}" \
+         --form-string "message=$1" \
+         https://api.pushover.net/1/messages.json &> /dev/null
+}
 
 git clone https://${DEPLOYMENT_KEY}@github.com/njkli/systems.git
 cd systems
 
-echo '******************************'
-nix build --help
-# nix --version
-echo '******************************'
-
 for system in $(nix eval .#nixosConfigurations --apply builtins.attrNames --json | jq -r 'join("\n")')
 do
-    echo '******************************'
-    echo ${system}
-    echo '******************************'
+    echo "****************************** Building: ${system} ******************************"
     nix-store --query --references $(nix build .#nixosConfigurations.${system}.config.system.build.toplevel --json | jq -r 'map(.drvPath) | join("\n")') \
         | xargs nix-store --realise \
         | xargs nix-store --query --requisites \
         | cachix push njk
-    echo '******************************'
+    echo "****************************** Cleaning nix-store: ${system} ******************************"
+    sudo nix-collect-garbage -d
+    nix-collect-garbage -d
+    echo "****************************** Done: ${system} ******************************"
+    _msg "Done with ${system}"
 done
-
-# mkdir -p $HOME/.ssh && \
-#     echo $DEPLOYMENT_KEY > $HOME/.ssh/id_rsa && \
-#     echo 'StrictHostKeyChecking no' > $HOME/.ssh/config && \
-#     chmod 0600 $HOME/.ssh/id_rsa && \
-#     cat $HOME/.ssh/id_rsa && \
