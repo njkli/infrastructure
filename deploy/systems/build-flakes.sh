3f353f11519e53915b578948c9c4ba5d077ffbae
Voob of Doom <vod@njk.li>
2020-12-09 13:15:14 +0100

    try1

---
+++ deploy/systems/build-flakes.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p git jq findutils bash
+
+mkdir -p $HOME/.ssh
+echo $DEPLOYMENT_KEY > $HOME/.ssh/id_rsa
+echo 'StrictHostKeyChecking no' > $HOME/.ssh/config
+chmod 0600 $HOME/.ssh/id_rsa
+
+git clone git@github.com:njkli/systems.git
+cd systems
+
+for system in $(nix eval .#nixosConfigurations --apply builtins.attrNames --json | jq -r 'join("\n")')
+do
+    nix-store --query --references \
+              $(nix build .#nixosConfigurations.${system}.config.system.build.toplevel --json | jq -r 'map(.drvPath) | join("\n")') \
+        | xargs nix-store --realise \
+        | xargs nix-store --query --requisites \
+        | cachix push njk
+done
