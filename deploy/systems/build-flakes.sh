1817a68a82b6f9d0671ee3d862d06c3be1c49b24
Voob of Doom <vod@njk.li>
2020-12-09 14:21:16 +0100

    again, FUCK NIX

--- deploy/systems/build-flakes.sh
+++ deploy/systems/build-flakes.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p git jq findutils bash
+#! nix-shell -i bash -p git jq findutils bash nixFlakes
 
 git clone https://${DEPLOYMENT_KEY}@github.com/njkli/systems.git
 cd systems
@@ -14,8 +14,7 @@ do
     echo '******************************'
     echo ${system}
     echo '******************************'
-    nix-store --query --references \
-              $(nix build .#nixosConfigurations.${system}.config.system.build.toplevel --json | jq -r 'map(.drvPath) | join("\n")') \
+    nix-store --query --references $(nix build .#nixosConfigurations.${system}.config.system.build.toplevel --json | jq -r 'map(.drvPath) | join("\n")') \
         | xargs nix-store --realise \
         | xargs nix-store --query --requisites \
         | cachix push njk
