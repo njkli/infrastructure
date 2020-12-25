6139b8f251658180f7aac722805a690782c241cf
Voob of Doom <vod@njk.li>
2020-12-25 12:30:26 +0100

    build straight from flake

--- deploy/systems/build-flakes.sh
+++ deploy/systems/build-flakes.sh
@@ -9,21 +9,19 @@ _msg() {
          https://api.pushover.net/1/messages.json &> /dev/null
 }
 
-git clone https://${DEPLOYMENT_KEY}@github.com/njkli/systems.git
-cd systems
+flake_url="git+https://${DEPLOYMENT_KEY}@github.com/njkli/systems"
 
-for system in $(nix eval .#nixosConfigurations --apply builtins.attrNames --json | jq -r 'join("\n")')
+for system in $(nix eval ${flake_url}#nixosConfigurations --apply builtins.attrNames --json | jq -r 'join("\n")')
 do
     echo "****************************** Building: ${system} ******************************"
-    nix-store --query --references $(nix build .#nixosConfigurations.${system}.config.system.build.toplevel --json | jq -r 'map(.drvPath) | join("\n")') \
+    nix-store --query --references $(nix build ${flake_url}#nixosConfigurations.${system}.config.system.build.toplevel --json | jq -r 'map(.drvPath) | join("\n")') \
         | xargs nix-store --realise \
         | xargs nix-store --query --requisites \
-        | cachix push njk
+        | cachix push njk && _msg "Passed: ${system}" || _msg "Failed: ${system}"
     echo "****************************** Cleaning nix-store: ${system} ******************************"
     # sudo nix-collect-garbage -d
     nix-collect-garbage -d
     echo "****************************** Done: ${system} ******************************"
-    _msg "Done with ${system}"
 done
 
 # except for marvin
